dafc29145959094a7dbced4d8454cda1307ddb2d
DemianK <demiankrige@gmail.com>
2016-06-16 20:25:38 -0600

    Authorization test

--- grails-app/controllers/pro3/com/pro3/type/StrategyController.groovy
+++ grails-app/controllers/pro3/com/pro3/type/StrategyController.groovy
@@ -1,7 +1,9 @@
 package pro3.com.pro3.type
 
 import com.pro3.type.Strategy
+import grails.plugin.springsecurity.annotation.Secured
 
+@Secured(['ROLE_ADMIN'])
 class StrategyController {
     static scaffold = Strategy
 }
